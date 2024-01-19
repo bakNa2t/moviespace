2681f2f8b68b2ba9590394c0058381faf8cd4508
bakNa2t <ok2.lock@gmail.com>
2024-01-19 11:32:38 +0300

    Update displayNavLinkActive function innerHTML href prop

--- docs/js/displayFunctions.js
+++ docs/js/displayFunctions.js
@@ -17,7 +17,7 @@ export async function displayMostWatchedMovies() {
     const elemDiv = document.createElement("div");
     elemDiv.classList.add("card");
     elemDiv.innerHTML = `
-      <a href="pages/movie-details.html?id=${movie.id}">
+      <a href="movie-details.html?id=${movie.id}">
         ${
           movie.poster_path
             ? `<img
@@ -35,7 +35,7 @@ export async function displayMostWatchedMovies() {
       <div class="card-vote">${movie.vote_average.toFixed(1)}</div>
       <div class="card-body">
         <h5 class="card-title">
-          <a href="pages/movie-details.html?id=${movie.id}">${movie.title}</a>
+          <a href="movie-details.html?id=${movie.id}">${movie.title}</a>
         </h5>
         <p class="card-text">
           <small class="text-muted">Release date: <span>${
