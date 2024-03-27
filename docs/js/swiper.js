b7037ce44cfe9260e079fa18ebcde00e1bf54546
bakNa2t <ok2.lock@gmail.com>
2024-03-27 12:22:33 +0300

    Added displaySwiperPopupTrailer func clicking icon using 'click' event listener display popup trailer

--- docs/js/swiper.js
+++ docs/js/swiper.js
@@ -96,6 +96,24 @@ export async function displayNowWatchingMoviesSlider() {
     document.querySelector(".swiper-wrapper").appendChild(elemDiv);
 
     initSwiper();
+
+    // Click on play icon to display popup with trailer
+    document.querySelectorAll(".fa-circle-play").forEach((item) => {
+      item.addEventListener("click", async () => {
+        const id = item.nextElementSibling.getAttribute("href").split("=")[1];
+
+        await displaySwiperPopupTrailer("movie", id);
+        displayPopup();
+      });
+    });
+    // Close popup modal when clicked on backdrop
+    document.querySelector(".popup").addEventListener("click", (e) => {
+      if (e.target === document.getElementById("popup__trailer")) hidePopup();
+    });
+    // Close popup modal when clicked on Escape
+    document.addEventListener("keydown", (e) => {
+      if (e.key === "Escape") hidePopup();
+    });
   });
 }
 
