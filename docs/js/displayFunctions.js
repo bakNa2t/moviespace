74b00e9696330a76193a1636b89d400b52c67d9a
bakNa2t <ok2.lock@gmail.com>
2024-03-29 13:03:43 +0300

    Added displayPopularContentPopupTrailer func to displayMostWatchedMovies/TVShows func

--- docs/js/displayFunctions.js
+++ docs/js/displayFunctions.js
@@ -140,6 +140,24 @@ export async function displayMostWatchedMovies() {
       </div>`;
 
     document.querySelector("#popular__content").appendChild(elemDiv);
+
+    // Click on play icon to display popup with trailer
+    document.querySelectorAll(".fa-circle-play").forEach((item) => {
+      item.addEventListener("click", async () => {
+        const id = item.nextElementSibling.getAttribute("href").split("=")[1];
+
+        await displayPopularContentPopupTrailer("movie", id);
+        displayPopup();
+      });
+    });
+    // Close popup modal when clicked on backdrop
+    document.querySelector(".popup").addEventListener("click", (e) => {
+      if (e.target === document.getElementById("popup__trailer")) hidePopup();
+    });
+    // Close popup modal when clicked on Escape
+    document.addEventListener("keydown", (e) => {
+      if (e.key === "Escape") hidePopup();
+    });
   });
 }
 
@@ -180,6 +198,24 @@ export async function displayMostWatchedTVShoes() {
       </div>`;
 
     document.querySelector("#popular__tv-content").appendChild(elemDiv);
+
+    // Click on play icon to display popup with trailer
+    document.querySelectorAll(".fa-circle-play").forEach((item) => {
+      item.addEventListener("click", async () => {
+        const id = item.nextElementSibling.getAttribute("href").split("=")[1];
+
+        await displayPopularContentPopupTrailer("tv", id);
+        displayPopup();
+      });
+    });
+    // Close popup modal when clicked on backdrop
+    document.querySelector(".popup").addEventListener("click", (e) => {
+      if (e.target === document.getElementById("popup__trailer")) hidePopup();
+    });
+    // Close popup modal when clicked on Escape
+    document.addEventListener("keydown", (e) => {
+      if (e.key === "Escape") hidePopup();
+    });
   });
 }
 
